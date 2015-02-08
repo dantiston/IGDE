e81f09441219864c51c26911a9ae25aa19f996a7
T.J. Trimble <trimblet@me.com>
2015-02-07 17:46:58 -0800

    Starting on IGDE development!

--- core/models.py
+++ core/models.py
@@ -1,6 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Comments(models.Model):
-    user = models.ForeignKey(User)
-    text = models.CharField(max_length=255)
+# Create your models here.
