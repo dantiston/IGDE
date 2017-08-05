eebd9382fa70bb30a76d08a1dd6afe777bee0bdc
T.J. Trimble <dantiston@gmail.com>
2017-08-05 15:28:23 -0700

    Archiving ace.py

--- igde/ace.py
+++ igde/ace.py
@@ -25,35 +25,22 @@ class InteractiveAce(AceParser):
 
     # TODO: Command doesn't run bash, can't use redirects
     #LUI_COMMANDS = ["-l", "--lui-fd=3", "--input-from-lui", "3<&0", "3>&1"]
-    LUI_COMMANDS = ["-l", "--lui-fd=3", "--input-from-lui"]
+    #LUI_COMMANDS = ["-l", "--lui-fd=3", "--input-from-lui"]
+    #LUI_COMMANDS = ["ace", "-l", "--lui-fd=3", "--input-from-lui", "3<&0", "3>&1"]
+    #LUI_COMMANDS = "-l --lui-fd={fd} --input-from-lui {fd}<&0 {fd}>&1"
+    LUI_COMMAND = "\"ace -g {grammar} -l --lui-fd={fd} --input-from-lui {fd}<&0 {fd}>&1\""
+
 
     def __init__(self, grm, cmdargs=None, executable=None, env=None,
                  tsdbinfo=False, **kwargs):
-        if cmdargs:
-            cmdargs.extend(self.LUI_COMMANDS)
-        else:
-            cmdargs = list(self.LUI_COMMANDS)
+        #if cmdargs:
+        #    cmdargs.extend(self.LUI_COMMANDS)
+        #else:
+        #    cmdargs = list(self.LUI_COMMANDS)
         tsdbinfo = False
+        #executable = 'bash'
         super().__init__(grm, cmdargs, executable, env, tsdbinfo, **kwargs)
         self.receive = self._igde_receive
-
-
-    def send(self, datum):
-        """
-        Send *datum* (e.g. a sentence or MRS) to ACE.
-        """
-        try:
-            #self._p.stdin.write("parse {}\f".format(datum))
-            #self._p.stdin.flush()
-            self._p.communicate("parse {}\f".format(datum))
-        except (IOError, OSError):  # ValueError if file was closed manually
-            logging.info(
-                'Attempted to write to a closed process; attempting to reopen'
-            )
-            self._open()
-            #self._p.stdin.write("parse {}\f".format(datum))
-            #self._p.stdin.flush()
-            self._p.communicate("parse {}\f".format(datum))
 
 
     def _igde_receive(self):
@@ -66,56 +53,53 @@ class InteractiveAce(AceParser):
         return response
 
 
-    def _result_lines(self, termini=None):
-        poll = self._p.poll
-        next_line = self._p.stdout.readline # TODO: This is a PROBLEM
-
-        if termini is None:
-            termini = self._termini
-        i, end = 0, len(termini)
-        cur_terminus = termini[i]
-
-        lines = []
-        while i < end:
-            s = next_line()
-            if s == '' and poll() != None:
-                logging.info(
-                    'Process closed unexpectedly; attempting to reopen'
-                )
-                self.close()
-                self._open()
-                break
-            else:
-                lines.append(s.rstrip())
-                if cur_terminus.search(s):
-                    i += 1
-        return [line for line in lines if line != '']
-
-
     # TODO: This isn't working
     def _open(self):
-        old_tty = termios.tcgetattr(sys.stdin)
-        tty.setraw(sys.stdin.fileno())
-        master_fd, slave_fd = pty.openpty()
+        #fd = os.fdopen(3)
+        #print(fd)
+
+        in_fd, out_fd = os.pipe()
+
+        #print(" ".join([self.executable, '-g', self.grm, self.LUI_COMMANDS.format(fd=in_fd)]))
+        print(self.LUI_COMMAND.format(fd=in_fd, grammar=self.grm))
         self._p = Popen(
-            [self.executable, '-g', self.grm] + self._cmdargs + self.cmdargs,
-            stdin=slave_fd,
-            stdout=slave_fd,
+            #" ".join([self.executable, '-g', self.grm] + self._cmdargs + self.cmdargs),
+            #" ".join([self.executable, '-g', self.grm, self.LUI_COMMANDS.format(fd=in_fd)]),
+            self.LUI_COMMAND.format(fd=in_fd, grammar=self.grm),
+            #stdin=PIPE,
+            #stdout=PIPE,
+            #stdin=3,
+            #stdout=3,
+            stdin=in_fd,
+            stdout=in_fd,
             env=self.env,
-            universal_newlines=True,
-            close_fds=False,
-            pass_fds=(master_fd, slave_fd)
+            #close_fds=False,
+            shell=True,
+            pass_fds=[in_fd, out_fd]
         )
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_tty)
+
+#        old_tty = termios.tcgetattr(sys.stdin)
+#        tty.setraw(sys.stdin.fileno())
+#        master_fd, slave_fd = pty.openpty()
+#        self._p = Popen(
+#           [self.executable, '-g', self.grm] + self._cmdargs + self.cmdargs,
+#           stdin=slave_fd,
+#           stdout=slave_fd,
+#           env=self.env,
+#           universal_newlines=True,
+#           close_fds=False,
+#           pass_fds=(master_fd, slave_fd)
+#        )
+#        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_tty)
 
 
-    def close(self):
-        """
-        Close the ACE process.
-        """
-        self._p.stdin.close()
-        for line in self._p.stdout:
-            logging.debug('ACE cleanup: {}'.format(line.rstrip()))
-        self._p.stdout.close()
-        retval = self._p.wait()
-        return retval
+#     def close(self):
+#         """
+#         Close the ACE process.
+#         """
+#         self._p.stdin.close()
+#         for line in self._p.stdout:
+#             logging.debug('ACE cleanup: {}'.format(line.rstrip()))
+#         self._p.stdout.close()
+#         retval = self._p.wait()
+#         return retval
